092acf57330516947da541e1f600364e5f261374
adfar <adf5092@gmail.com>
2025-11-24 19:02:45 -0500

    Initial Commit

---
+++ ReaderTests/ReaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderTests.swift
+//  ReaderTests
+//
+//  Created by Aidan Farnum on 11/24/25.
+//
+
+import Testing
+@testable import Reader
+
+struct ReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
